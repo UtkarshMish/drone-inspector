d243b78e195ab584c29529587d5749d56e96ee99
Utkarsh Mishra <iiutkarshmishraii@gmail.com>
2021-11-21 20:47:51 +0530

    added mongo URI

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -6,7 +6,8 @@ load_dotenv(".env")
 
 
 DB_NAME = "drone_tracker"
-HOST_IP = "localhost"
+HOST_IP = environ.get("HOST_IP")
 DB_PORT = 27017
 SECRET_KEY = environ.get("SECRET_KEY")
 DEBUG = True if environ.get("DEBUG") == 'true' else False
+MONGO_URI = environ.get("CONNECTION_URI")
--- app/database/__init__.py
+++ app/database/__init__.py
@@ -2,7 +2,7 @@
 
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from app.config import DB_NAME, HOST_IP, DB_PORT
+from app.config import DB_NAME, HOST_IP, DB_PORT, MONGO_URI
 from .Drones import Drones
 from .DroneTypes import DroneTypes
 from .Pilots import Pilots
@@ -10,6 +10,6 @@ from .Pilots import Pilots
 
 async def init_DB():
     client = AsyncIOMotorClient(
-        f"mongodb://{HOST_IP}:{DB_PORT}"
+        f"mongodb+srv://recipe_project://{HOST_IP}:{DB_PORT}" if not MONGO_URI else MONGO_URI
     )
     await init_beanie(database=client[DB_NAME], document_models=[Drones, DroneTypes, Pilots])
