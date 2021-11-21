4bf327c2a6ee055ba5f7dbab18c7020b96f97700
Utkarsh Mishra <iiutkarshmishraii@gmail.com>
2021-11-21 21:10:22 +0530

    updated build files

--- drone-inspector-app/src/utils/droneInfo.js
+++ drone-inspector-app/src/utils/droneInfo.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
 export async function getDroneInfo(pageNo) {
-  const { data } = await axios.get(`/api/drones?page=${parseInt(pageNo)}`);
-  if (data && data.result && data.total_pages) {
-    const { result, total_pages } = data;
-    return [result, total_pages];
+  try {
+    const { data } = await axios.get(`/api/drones?page=${parseInt(pageNo)}`);
+
+    if (data && data.result && data.total_pages) {
+      const { result, total_pages } = data;
+      return [result, total_pages];
+    }
+  } catch (error) {
+    console.log(error);
   }
-  return null;
+  return [[], 0];
 }
